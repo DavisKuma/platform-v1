501e93a3f3707f7d1560381964e273bc09fd54dd
waqarmm <wiki.mir@gmail.com>
2026-04-20 22:00:00 +0500

    api

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE = "/api";
+const BASE = "https://platform-zqev.onrender.com/api";
 
 async function request<T>(path: string, opts?: RequestInit): Promise<T> {
   const res = await fetch(`${BASE}${path}`, {
